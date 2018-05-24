cdd2c2cd42b74ca2d027c6ce5c5dae442191c497
Steven Emmanuel <ksteven925@gmail.com>
2018-05-25 04:27:42 +0530

    T07
    added functionality for spare & strike scenarios where scoring is determined after further rolls.

--- ScoreMaster.cs
+++ ScoreMaster.cs
@@ -21,19 +21,47 @@ public class ScoreMaster {
 	// Return a list of individual frame scores, NOT cumulative.
 	public static List<int> ScoreFrames (List<int> rolls) {
 		List<int> frameList = new List<int> ();
-		int total = 0,Count = 0;
 
 		//your code here
 
 		int rollCount = rolls.Count;	//length of the rolls list
-		if (rollCount % 2 != 0) {
-			rolls.RemoveAt (rollCount-1);
+		if (rollCount % 2 != 0) {		//check if the last frame is completed or not
+			rolls.RemoveAt (rollCount-1);	//removing the incomplete frame from the score calculation as the turn is not finished
 		}
+
+		int total = 0,Count = 0;
+		bool onSecondFrame = false,aSpare = false,aStrike = false;
 
 		foreach(int roll in rolls){
 			Count++;
+			onSecondFrame = false;
+			if(Count % 2 == 0){ onSecondFrame = true;}
+
 			total += roll;
-			if (Count % 2 == 0) {
+
+			if(aStrike){
+				aStrike = false;
+				continue;
+			}
+			if(aSpare){
+				aSpare = false;
+				frameList.Add (total);
+				total = 0;
+			}
+
+			if(total == 10){	//strike or spare
+				if(onSecondFrame){	//spare
+					//spare stuff
+					aSpare = false;
+					continue;
+				}
+				if( ! onSecondFrame){	//a strike!!
+					aStrike = true;
+					Count++;
+					continue;
+				}
+			}
+			if (onSecondFrame) {
 				frameList.Add (total);
 				total = 0;
 			}
