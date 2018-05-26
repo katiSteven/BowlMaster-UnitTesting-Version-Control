824ae7868e8fc2359358b0e82222a8c3adbca6f5
Steven Emmanuel <ksteven925@gmail.com>
2018-05-27 00:43:28 +0530

    Done (Total Refactor)
    Previous solutions were commented.
    Old solutions were a scraped.
    Did a total refactor.

--- ScoreMaster.cs
+++ ScoreMaster.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+﻿//using System.Collections;
 using System.Collections.Generic;
 //using System.
 using UnityEngine;
+using System;
 
 
 public class ScoreMaster {
@@ -11,10 +12,14 @@ public class ScoreMaster {
 
 		List<int> cumulativeScores = new List<int> ();
 		int runningTotal = 0;
+		int frameCount = 0;// should be no more than 10 frames
 
 		foreach(int frameScore in ScoreFrames (rolls)){
-			runningTotal += frameScore;
-			cumulativeScores.Add (runningTotal);
+			frameCount++;
+			if(frameCount <= 10){
+				runningTotal += frameScore;
+				cumulativeScores.Add (runningTotal);
+			}
 		}
 
 		return cumulativeScores;
@@ -26,66 +31,93 @@ public class ScoreMaster {
 
 		//your code here
 
-		int total = 0,Count = 0;
-		bool onSecondFrameCurrently = false;
-		bool wasASpare = false;
+//		int total = 0,Count = 0;
+//		bool onSecondFrameCurrently = false;
+//		bool wasASpare = false;
+//
+//		int bowlsAfterSpare = 0;
+//		int bowlsAfterStrike = 0;
+//
+//		Stack StrikeStack = new Stack();
+//		StrikeStack.Push ("$");
+//
+//		foreach(int roll in rolls){
+//			Count++;
+//			onSecondFrameCurrently = false;
+//			if(Count % 2 == 0){ onSecondFrameCurrently = true;}
+//
+//			total += roll;
+//
+//
+//			if((total % 10) == 0){	// spare
+//				if(onSecondFrameCurrently){	//spare
+//					wasASpare = true;
+//					continue;
+//				}
+//			}
+//
+//
+//
+//
+//			if(wasASpare)
+//				bowlsAfterSpare++;
+//
+//			if (StrikeStack.Peek ().ToString().Equals("S")) {
+//				bowlsAfterStrike++;
+//			}
+//
+//
+//			if(bowlsAfterSpare == 1){
+//				wasASpare = false; bowlsAfterSpare = 0;
+//				frameList.Add (total);
+//				total = roll;
+//
+//			}else if((bowlsAfterStrike % 2) == 0 && bowlsAfterStrike != 0){
+//				bowlsAfterStrike = 0;
+//				StrikeStack.Pop ();
+//				frameList.Add (total);
+//				total = total - 10;
+//
+//			} if (onSecondFrameCurrently && total <10) {
+//				frameList.Add (total);
+//				total = 0;
+//			}
+//
+//			if( roll == 10){	//a strike!!
+//				StrikeStack.Push ("S");
+//				Count--;
+//				//				continue;
+//			}
+//
+//
+//		} 
 
-		int bowlsAfterSpare = 0;
-		int bowlsAfterStrike = 0;
 
-		Stack StrikeStack = new Stack();
-		StrikeStack.Push ("$");
-
-		foreach(int roll in rolls){
-			Count++;
-			onSecondFrameCurrently = false;
-			if(Count % 2 == 0){ onSecondFrameCurrently = true;}
-
-			total += roll;
-
-
-			if((total % 10) == 0){	// spare
-				if(onSecondFrameCurrently){	//spare
-					wasASpare = true;
-					continue;
+			int total = 0;//stores the total points for each frame(sum of 1st & 2nd bowl)
+			int count = 0;//to keep track of every bowl
+		try{
+			for(int i=0;i<rolls.Count;i++){
+				count++;
+				total += rolls [i];
+				if (rolls [i] == 10 && count % 2 == 1) {	//strike conditions
+					total += rolls [i + 1] + rolls [i + 2];
+					frameList.Add(total);
+					total = 0;				//reset the total
+					count++;
+				}
+				else if(total == 10 && (count % 2) == 0){	//spare conditions
+					total += rolls[i + 1];
+					frameList.Add(total);
+					total = 0;
+				}
+				else if(rolls[i] < 10 && (count % 2) == 0){		//every frame has 2 attempts where pin(s) are left standing
+					frameList.Add(total);
+					total = 0;				//reset the total
 				}
 			}
-
-			if( roll == 10){	//a strike!!
-				StrikeStack.Push ("S");
-				Count++;
-				continue;
-			}
-
-
-			if(wasASpare)
-				bowlsAfterSpare++;
-
-			if (StrikeStack.Peek ().ToString().Equals("S")) {
-				bowlsAfterStrike++;
-			}
-
-
-			if(bowlsAfterSpare == 1){
-				wasASpare = false; bowlsAfterSpare = 0;
-				frameList.Add (total);
-				total = roll;
-
-			}else if((bowlsAfterStrike % 2) == 0 && bowlsAfterStrike != 0){
-				bowlsAfterStrike = 0;
-				StrikeStack.Pop ();
-				frameList.Add (total);
-				total = total - 10;
-
-			} if (onSecondFrameCurrently && total <10) {
-				frameList.Add (total);
-				total = 0;
-			}
-
-
+		}catch(ArgumentOutOfRangeException){
+			return frameList;
 		}
-
-
 		return frameList;
 	}
 }
