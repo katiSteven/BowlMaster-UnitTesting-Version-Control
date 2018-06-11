af3a003956638ec37fb7c4646dc3c4aff7b5f818
Steven Emmanuel <ksteven925@gmail.com>
2018-06-12 05:07:43 +0530

    alternate solution (Ben Tristem)
    This version contains Ben Tristam's version of ScoreMaster.cs used in the Complete Unity Course on Udemy.

--- ScoreMaster.cs
+++ ScoreMaster.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using System;
 
 
-public class ScoreMaster {
+public static class ScoreMaster {
 
 	//returns a list of cumulative scores, like a normal score card.
 	public static List <int> ScoreCumulative (List <int> rolls){
@@ -20,31 +20,49 @@ public class ScoreMaster {
 
 	// Return a list of individual frame scores, NOT cumulative.
 	public static List<int> ScoreFrames (List<int> rolls) {
-		List<int> frameList = new List<int> ();
-			int total = 0;//stores the total points for each frame(sum of 1st & 2nd bowl)
-			int count = 0;//to keep track of every bowl
-		try{
-			for(int i=0;i<rolls.Count;i++){
-				if(frameList.Count == 10){break;}
-				count++;					//to consider every frame
-				total += rolls [i];			//adding every frame score
-				if (rolls [i] == 10 && count % 2 == 1) {	//strike conditions
-					frameList.Add(total + rolls [i + 1] + rolls [i + 2]);
-					total = 0;				//reset the total
-					count++;				//to take into consideration the skipped 2nd attempt during a strike
-				}
-				else if(total == 10 && (count % 2) == 0){	//spare conditions
-					frameList.Add(total + rolls[i + 1]);
-					total = 0;				//reset the total
-				}
-				else if(rolls[i] < 10 && (count % 2) == 0){		//Normal "OPEN" frame
-					frameList.Add(total);
-					total = 0;				//reset the total
-				}
+		List<int> frames = new List<int> ();
+//			int total = 0;//stores the total points for each frame(sum of 1st & 2nd bowl)
+//			int count = 0;//to keep track of every bowl
+//		try{
+//			for(int i=0;i<rolls.Count;i++){
+//				if(frameList.Count == 10){break;}
+//				count++;					//to consider every frame
+//				total += rolls [i];			//adding every frame score
+//				if (rolls [i] == 10 && count % 2 == 1) {	//strike conditions
+//					frameList.Add(total + rolls [i + 1] + rolls [i + 2]);
+//					total = 0;				//reset the total
+//					count++;				//to take into consideration the skipped 2nd attempt during a strike
+//				}
+//				else if(total == 10 && (count % 2) == 0){	//spare conditions
+//					frameList.Add(total + rolls[i + 1]);
+//					total = 0;				//reset the total
+//				}
+//				else if(rolls[i] < 10 && (count % 2) == 0){		//Normal "OPEN" frame
+//					frameList.Add(total);
+//					total = 0;				//reset the total
+//				}
+//			}
+//		}catch(ArgumentOutOfRangeException){
+//			return frameList;
+//		}
+
+		// Index i points to 2nd bowl of frame
+		for (int i = 1; i < rolls.Count; i += 2) {
+			if (frames.Count == 10) {break;}				// Prevents 11th frame score
+
+			if (rolls[i-1] + rolls[i] < 10) {				// Normal "OPEN" frame
+				frames.Add (rolls [i-1] + rolls [i]);
 			}
-		}catch(ArgumentOutOfRangeException){
-			return frameList;
+
+			if (rolls.Count - i <= 1) {break;}				// Ensure at least 1 look-ahead available
+
+			if (rolls[i-1] == 10) {							
+				i--;										// STRIKE frame has just one bowl
+				frames.Add (10 + rolls [i+1] + rolls[i+2]);
+			} else if (rolls[i-1] + rolls[i] == 10) {		// SPARE bonus
+				frames.Add (10 + rolls [i+1]);
+			}
 		}
-		return frameList;
+		return frames;
 	}
 }
